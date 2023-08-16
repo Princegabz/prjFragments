ec5777b58b1ef8ae19e7842abcf7a11b714df203
Reece Wanvig <rwanvig@iie.ac.za>
2023-08-16 10:16:33 +0200

    Updated to have frag bar
    I have added the fragment bar

---
+++ app/src/main/java/com/example/prjfragments/MenuBar.kt
@@ -0,0 +1,36 @@
+package com.example.prjfragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+private val Frag1 = FirstFragment()
+private val Frag2= SecondFragment()
+class MenuBar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_bar)
+        replaceFrag(Frag1)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.NavBar)
+        bottomNav.setOnItemSelectedListener {
+            when(it.itemId)
+            {
+                R.id.ic_home->replaceFrag(Frag1)
+                R.id.ic_settings->replaceFrag(Frag2)
+            }
+            true
+        }
+
+
+    }
+    private fun replaceFrag(fragment: Fragment)
+    {
+        if (fragment!=null)
+        {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.frameLayout,fragment)
+            transaction.commit()
+        }
+    }
+}
